038ac85dcdc3b243d59fc032fa0189e7d227f566
vampironok <myasnikovakate@gmail.com>
2017-10-15 12:26:25 +0400

    Added second task - synchronization by concurrent package

--- Bank/src/main/java/com/epam/lab/bank/lock/BankUser.java
+++ Bank/src/main/java/com/epam/lab/bank/lock/BankUser.java
@@ -1,4 +1,34 @@
 package com.epam.lab.bank.lock;
 
-public class BankUser {
+import com.epam.lab.bank.Bank;
+
+import java.util.concurrent.locks.Lock;
+
+public class BankUser implements Runnable {
+    private static final int AMOUNT_TO_GET = 10;
+    private Bank bank;
+    private Lock lock;
+
+    public BankUser(Bank bank, Lock lock) {
+        this.bank = bank;
+        this.lock = lock;
+    }
+
+    public void run() {
+        boolean canContinue = true;
+        while (canContinue) {
+            lock.lock();
+            try {
+                //System.out.println(Thread.currentThread() + " get lock");
+                if (this.bank.hasMoney(AMOUNT_TO_GET)) {
+                    this.bank.getMoney(AMOUNT_TO_GET);
+                } else {
+                    canContinue = false;
+                }
+            } finally {
+                //System.out.println("Thread-" + Thread.currentThread() + " realize lock. Can continue? " + canContinue);
+                lock.unlock();
+            }
+        }
+    }
 }
